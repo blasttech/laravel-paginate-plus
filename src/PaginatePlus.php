b0d8168f6f5af8ddfa314a47a1b47b96b4b2febd
Michael Blair <mhkblair@gmail.com>
2018-02-01 13:18:21 +1100

    Initial commit

---
+++ src/PaginatePlus.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Blasttech\PaginatePlus;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+/**
+ * Interface PaginatePlus
+ *
+ * @package Blasttech\PaginatePlus
+ */
+interface PaginatePlus
+{
+    /**
+     * @param Builder $query
+     * @param int|null $per_page
+     * @return LengthAwarePaginator
+     */
+    public function scopePaginatePlus(Builder $query, $per_page = null);
+}
---
+++ src/PaginatePlusTrait.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Blasttech\PaginatePlus;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Input;
+
+/**
+ * Trait PaginatePlusTrait
+ *
+ * @package Blasttech\PaginatePlus
+ */
+trait PaginatePlusTrait
+{
+    /**
+     * @param Builder $query
+     * @param int|null $per_page
+     * @return LengthAwarePaginator
+     */
+    public function scopePaginatePlus(Builder $query, $per_page = null)
+    {
+        $paginate = $per_page ?? $query->getModel()->getPerPage();
+        $page = Input::get('page', 1);
+
+        $queryCount = (clone $query);
+        $total = DB::connection($queryCount->connection)
+            ->table(DB::raw("({$queryCount->toSql()}) as sub"))
+            ->mergeBindings($queryCount->getQuery())
+            ->count();
+
+        $offSet = ($page * $paginate) - $paginate;
+        $itemsForCurrentPage = array_slice($query->get()->toArray(), $offSet, $paginate, true);
+        $result = new LengthAwarePaginator(
+            $itemsForCurrentPage,
+            $total,
+            $paginate,
+            $page
+        );
+        return $result;
+    }
+}
